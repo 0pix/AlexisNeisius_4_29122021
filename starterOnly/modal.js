00cd9684b662db218e37092ba7dcca987932fc27
Dagueron <opixgamer@gmail.com>
2022-01-04 02:14:39 +0100

    I started the validated function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,6 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
+  console.log(x);
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,13 +12,51 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+
+// launch modal form
+const launchModal = () => {
+  modalbg.style.display = "block";
+};
+
+const closeModal = () => {
+  modalbg.style.display = "none";
+};
+
+// //Close modal form
+// function closeModal() {
+//   modalbg.style.display = "none";
+// }
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+//Close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
+//Submit
 
+const validate = () => {
+  let isValidate = true;
+  const firstName = document.getElementById("first");
+  const lastName = document.getElementById("last");
+  const email = document.getElementById("email");
+  const birthdate = document.getElementById("birthdate");
+  const quantity = document.getElementById("quantity");
+  const location = document.getElementsByName("location");
+  const conditions = document.querySelector(".checkbox-icon");
+
+  if (firstName.value.length < 4) {
+    isValidate = false;
+    alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+  }
+  console.log(email);
+};
+
+validate();
+//No refreh
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
