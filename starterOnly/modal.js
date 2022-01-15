ed2ae7dba8142792ea17f14cdbb5f435dc82a1b0
Dagueron <opixgamer@gmail.com>
2022-01-15 07:23:25 +0100

    rework function validate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -2,7 +2,6 @@ function editNav() {
   const x = document.getElementById("myTopnav");
   const menuTopNav = document.getElementsByClassName("icon");
   const iconTopNav = document.getElementById("iconTopNav");
-  console.log(iconTopNav);
   // console.log(x);
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -47,134 +46,87 @@ btnCloseModal.addEventListener("click", closeModal);
 
 //Submit function
 
+const validateBalise = document.getElementById("thank-form");
+const myRegx =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let isValidate;
+
 const validate = () => {
-  let isValidate = true;
-  const firstName = document.getElementById("first");
-  const lastName = document.getElementById("last");
-  const email = document.getElementById("email");
-  const birthDate = document.getElementById("birthdate");
-  const quantity = document.getElementById("quantity");
-  const location = document.getElementsByName("location");
-  const conditions = document.getElementById("checkbox1");
-  const errorMessage = document.getElementsByClassName("error-message");
-  const validateMessage = document.getElementById("thank-form");
-  const boutonValidate = document.getElementsByClassName("btn-submit");
-  const input = document.getElementsByTagName("input");
-  let inputNumber = [
-    firstName,
-    lastName,
-    email,
-    birthDate,
-    quantity,
-    location,
-    conditions,
-  ];
+  isValidate = true;
 
-  // Regx
-  //  https://emailregex.com/
-  const myRegx =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  for (let i = 0; i < formData.length; i++) {
+    const inputBalise = formData[i].querySelector("input");
+    const labelBalise = formData[i].querySelector("label");
+    const errorBalise = formData[i].querySelector(".error-message");
+    let errorMessage = "";
 
-  function falseValidation(inputNumber, message) {
-    isValidate = false;
-    errorMessage[inputNumber].style.display = "block";
-    errorMessage[inputNumber].innerHTML = message;
-    if ((input[inputNumber].type == "text", "email")) {
-      input[inputNumber].style.border = "3px red solid";
+    if (inputBalise.value === "") {
+      errorMessage = "Vous devez entrer votre " + labelBalise.textContent;
+      isValidate = false;
     }
-  }
 
-  function trueValidation(inputNumber) {
-    errorMessage[inputNumber].style.display = "none";
-    if ((input[inputNumber].type == "text", "email", "date", "number")) {
-      input[inputNumber].style.border = "3px green solid";
+    switch (inputBalise.type) {
+      case "text":
+        if (inputBalise.value.length < 2) {
+          errorMessage =
+            "Veuillez entrer 2 caractères ou plus pour le champ du " +
+            labelBalise.textContent;
+          isValidate = false;
+        }
+        break;
+      case "email":
+        if (myRegx.test(email.value) == false) {
+          errorMessage = "Votre email n'est pas valide.";
+          isValidate = false;
+        }
+        break;
+      case "number":
+        if (
+          typeof parseInt(inputBalise.value) !== "number" ||
+          inputBalise.value === ""
+        ) {
+          errorMessage = "Veuillez choisir un nombre.";
+          isValidate = false;
+        }
+        break;
+      case "radio":
+        let inputRadio = formData[i].querySelectorAll("input");
+        let radioValue;
+
+        for (let v = 0; v < inputRadio.length; v++) {
+          if (inputRadio[v].checked) {
+            radioValue = inputRadio[v].value;
+          }
+        }
+
+        if (radioValue === undefined) {
+          errorMessage = "Vous devez choisir une option.";
+          isValidate = false;
+        }
+        break;
+      case "checkbox":
+        if (inputBalise.checked === false) {
+          errorMessage =
+            "Vous devez vérifier que vous acceptez les termes et conditions.";
+          isValidate = false;
+        }
+        break;
     }
-  }
 
-  // FirstName
-
-  if (firstName.value.length < 2) {
-    falseValidation(
-      0,
-      "<div><p>Veuillez entrer 2 caractères ou plus pour le champ du prénom.</p></div>"
-    );
-  } else {
-    trueValidation(0);
-  }
-
-  // Name
-
-  if (lastName.value.length < 2) {
-    falseValidation(
-      1,
-      "<div><p>Veuillez entrer 2 caractères ou plus pour le champ du nom.</p></div>"
-    );
-  } else {
-    trueValidation(1);
-  }
-
-  // Email
-
-  if (email.value === "") {
-    falseValidation(2, "<div><p>Vous devez entrer votre email.</p></div>");
-  } else if (myRegx.test(email.value) == false) {
-    falseValidation(2, "<div><p>Votre email n'est pas valide.</p></div>");
-  } else {
-    trueValidation(2);
-  }
-
-  // Birth date
-
-  if (birthDate.value === "") {
-    falseValidation(
-      3,
-      "<div><p>Vous devez entrer votre date de naissance.</p></div>"
-    );
-  } else {
-    trueValidation(3);
-  }
-
-  // Quantity
-
-  if (quantity.value === "") {
-    falseValidation(4, "<div><p>Veuillez choisir un nombre.</p></div>");
-  } else if (typeof parseInt(quantity.value) != "number") {
-    falseValidation(4, "<div><p>Veuillez choisir un nombre.</p></div>");
-  } else {
-    trueValidation(4);
-  }
-
-  // Location
-
-  let valeur;
-  for (var i = 0; i < location.length; i++) {
-    if (location[i].checked) {
-      valeur = location[i].value;
+    if (errorMessage !== "") {
+      if (inputBalise.type !== "radio" && inputBalise.type !== "checkbox") {
+        inputBalise.style.border = "3px red solid";
+      }
+    } else {
+      inputBalise.style.border = "3px green solid";
     }
+
+    errorBalise.style.display = "block";
+    errorBalise.innerHTML = errorMessage;
   }
 
-  if (valeur === undefined) {
-    falseValidation(5, "<div><p>Vous devez choisir une option.</p></div>");
-  } else {
-    trueValidation(5);
-  }
-
-  // Conditions
-
-  if (conditions.checked === false) {
-    falseValidation(
-      6,
-      "<div><p>Vous devez vérifier que vous acceptez les termes et conditions.</p></div>"
-    );
-  } else {
-    trueValidation(6);
-  }
-
-  // Validate
-
-  if (isValidate === true) {
-    validateMessage.style.display = "flex";
-    boutonValidate[0].style.backgroundColor = "#279E7A";
+  if (isValidate) {
+    validateBalise.style.display = "flex";
   }
 };
 
