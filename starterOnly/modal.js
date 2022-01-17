b0b9d996b9910bbf3024fcfca44b856da0baff50
Dagueron <opixgamer@gmail.com>
2022-01-17 18:26:47 +0100

    update, small changes

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,5 @@
+/***************|fonction bouton nav|***************/
+
 function editNav() {
   const x = document.getElementById("myTopnav");
   const menuTopNav = document.getElementsByClassName("icon");
@@ -15,6 +17,7 @@ function editNav() {
     iconTopNav.style.color = "#ff0000";
   }
 }
+/***************|Open/Close modal|***************/
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -24,47 +27,40 @@ const closeBtn = document.querySelectorAll(".close");
 const btnCloseModal = document.getElementById("btn-close-modal");
 
 // launch modal form
-
 const launchModal = () => {
   modalbg.style.display = "block";
 };
 
 // Close Modal form
-
 const closeModal = () => {
   modalbg.style.display = "none";
 };
 
 // launch modal event
-
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 //Close modal event
-
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 btnCloseModal.addEventListener("click", closeModal);
 
-//Submit function
+/***************|Submit function|***************/
 
-const validateBalise = document.getElementById("thank-form");
+// Regx for Email
 const myRegx =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// Variable for validate message
 let isValidate;
+const validateBalise = document.getElementById("thank-form");
 
 const validate = () => {
   isValidate = true;
-
   for (let i = 0; i < formData.length; i++) {
     const inputBalise = formData[i].querySelector("input");
     const labelBalise = formData[i].querySelector("label");
     const errorBalise = formData[i].querySelector(".error-message");
     let errorMessage = "";
 
-    if (inputBalise.value === "") {
-      errorMessage = "Vous devez entrer votre " + labelBalise.textContent;
-      isValidate = false;
-    }
-
+    // Switch to give an action by type.
     switch (inputBalise.type) {
       case "text":
         if (inputBalise.value.length < 2) {
@@ -74,12 +70,14 @@ const validate = () => {
           isValidate = false;
         }
         break;
+
       case "email":
         if (myRegx.test(email.value) == false) {
           errorMessage = "Votre email n'est pas valide.";
           isValidate = false;
         }
         break;
+
       case "number":
         if (
           typeof parseInt(inputBalise.value) !== "number" ||
@@ -89,6 +87,7 @@ const validate = () => {
           isValidate = false;
         }
         break;
+
       case "radio":
         let inputRadio = formData[i].querySelectorAll("input");
         let radioValue;
@@ -104,6 +103,7 @@ const validate = () => {
           isValidate = false;
         }
         break;
+
       case "checkbox":
         if (inputBalise.checked === false) {
           errorMessage =
@@ -113,25 +113,33 @@ const validate = () => {
         break;
     }
 
+    // If message is empty
+    if (inputBalise.value === "") {
+      errorMessage = "Vous devez entrer votre " + labelBalise.textContent;
+      isValidate = false;
+    }
+
+    // Border color for other type than type radio and checkbox
     if (errorMessage !== "") {
       if (inputBalise.type !== "radio" && inputBalise.type !== "checkbox") {
         inputBalise.style.border = "3px red solid";
       }
     } else {
-      inputBalise.style.border = "3px green solid";
+      inputBalise.style.border = "3px #279e7a solid";
     }
 
+    // Show error message
     errorBalise.style.display = "block";
     errorBalise.innerHTML = errorMessage;
   }
 
+  //Global message validation
   if (isValidate) {
     validateBalise.style.display = "flex";
   }
 };
 
 //No refreh form
-
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
